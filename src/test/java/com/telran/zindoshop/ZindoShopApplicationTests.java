5f321c926f1687483b153364bdb2603c5df6c346
ikar-zindo <plag.ikar@gmail.com>
2023-11-14 01:03:14 +0100

    initial(): creation of the Zindo-shop project in Java

---
+++ src/test/java/com/telran/zindoshop/ZindoShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telran.zindoshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZindoShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
